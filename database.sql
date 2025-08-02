a2f85d523945ee6e079bd18fc7d2144a60a6e689
H24-D <d2876379@gmail.com>
2025-08-02 11:54:22 +0530

    Delete database.sql

--- database.sql
+++
@@ -1,9 +0,0 @@
--- CREATE DATABASE tracker_db;
-use tracker_db;
-CREATE TABLE book_tracker(
-id VARCHAR(36) PRIMARY KEY,
-title VARCHAR(255) NOT NULL,
-author VARCHAR(255) NOT NULL,
-status ENUM('read','reading','to-read') NOT NULL,
-review TEXT
-);
